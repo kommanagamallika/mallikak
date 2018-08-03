ccb02a80da8fd63593a246e28d14d54f06ccf0a3
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-08-03 14:02:52 +0530

    Update maximum.java

--- cse/maximum.java
+++ cse/maximum.java
@@ -1,9 +1,18 @@
- int n, max;
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		         int n, max;
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter number of elements in the array:");
         n = s.nextInt();
         int a[] = new int[n];
-        System.out.println("Enter elements of array:");
         for(int i = 0; i < n; i++)
         {
             a[i] = s.nextInt();
@@ -16,4 +25,6 @@
                 max = a[i];
             }
         }
-        System.out.println("Maximum value:"+max);
+        System.out.println(+max);
+	}
+}
