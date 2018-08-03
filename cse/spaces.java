2312c3ce16c06c61ef8aae4314b2866f715bd4b5
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-08-03 16:02:07 +0530

    Create spaces.java

---
+++ cse/spaces.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			String s;
+			Scanner in=new Scanner(System.in);
+			s=in.next();
+			 int words = 0;
+		 while(in.hasNext())
+	            {
+	                in.next();
+	                words++;
+	            }
+	            System.out.println((+words));
+		}
+	}
