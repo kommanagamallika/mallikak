8bd871eaa5c76091344f7bf81ccfb40e00dd4091
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-08-09 12:14:44 +0530

    Create 43.java

---
+++ cse/43.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s1,s2;
+		
+		Scanner s=new Scanner(System.in);
+		s1=s.next();
+		s2=s.next();
+		
+			System.out.println(s1+s2);
+
+	}
+}
