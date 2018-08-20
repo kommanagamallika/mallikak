813f7ec89dce4dfdc94eb5f58aedbdf8db3948cd
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-08-20 10:24:06 +0530

    Create 81.java

---
+++ cse/81.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int k,o;
+		Scanner sc=new Scanner(System.in);
+		k=sc.nextInt();
+		o=sc.nextInt();
+		if(k>=(Math.pow(2,32))&&(o>=Math.pow(2,32)))
+		{
+			System.out.print("End of File");
+			
+		}
+		else
+		{
+			System.out.print(o-k);
+		}
+		
+	}
+}
