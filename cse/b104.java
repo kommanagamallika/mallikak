41ca7d44dee64c928ad3a7313d3ed9ad869f73bb
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-08-24 12:28:41 +0530

    Rename 104.java to b104.java

---
+++ cse/b104.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   int n,k;
+	   Scanner sc=new Scanner(System.in);
+	   n=sc.nextInt();
+	   	   k=sc.nextInt();
+	   	   int pow=1;
+	   	   for(int i=1;i<=k;i++)
+	   	   {
+	   	   	pow=pow*n;
+	   	   }
+	   	   System.out.print(pow);
+
+	   
+	}
+}
