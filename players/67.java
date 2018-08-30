542605202760817718a8788170f6eb8a6d105c55
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-08-30 11:31:44 +0530

    Create 67.java

---
+++ players/67.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int num;
+		 Scanner sc=new Scanner(System.in);
+		 num=sc.nextInt();
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.println(factorial);
+    }
+}
