d1b74dc15cba271d268dfa170c2ba7c9783001c7
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-09-05 14:49:22 +0530

    Create 11.java

---
+++ players/11.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str;
+		Scanner sc=new Scanner(System.in);
+		str=sc.next().toLowerCase();
+		if(str.equals("sunday")||str.equals("saturday"))
+		{
+			System.out.println("yes");
+			
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
