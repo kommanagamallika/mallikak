f5e3e972a545a7e69b504ef350a26a8b30ec077a
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-09-12 09:35:36 +0530

    Update 92.java

--- players/92.java
+++ players/92.java
@@ -1,6 +1,17 @@
-Scanner s=new Scanner(System.in);
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
             
-            System.out.println("Enter a binary number:");
+            
             int n=s.nextInt();
             
             int decimal=0,p=0;
@@ -14,3 +25,4 @@ Scanner s=new Scanner(System.in);
             
             System.out.println(decimal);
         }
+}
