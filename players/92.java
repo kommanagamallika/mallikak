6b7619db50c3c840c872aab5d225af10a17f6e11
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-09-12 09:33:52 +0530

    Create 92.java

---
+++ players/92.java
@@ -0,0 +1,16 @@
+Scanner s=new Scanner(System.in);
+            
+            System.out.println("Enter a binary number:");
+            int n=s.nextInt();
+            
+            int decimal=0,p=0;
+            
+            while(n!=0)
+            {
+                decimal+=((n%10)*Math.pow(2,p));
+                n=n/10;
+                p++;
+            }
+            
+            System.out.println(decimal);
+        }
